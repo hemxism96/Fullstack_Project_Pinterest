0a6c2b107d64ef35367ea4ca34c77e000e0e663a
hemxism96 <josy6112@gmail.com>
2021-10-30 02:46:57 +0200

    add nginx

--- api/app/views.py
+++ api/app/views.py
@@ -5,3 +5,7 @@ views = Blueprint('views', __name__)
 @views.route('/')
 def home():
     return "<h1> HELLO WORLD!!!!!</h1>"
+
+@views.route('/hello')
+def hello():
+    return "<h1> HELLO WORLD2!!!!!</h1>"
