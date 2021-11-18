dddfb797747ed5123ce44befc6e725b03e548716
hemxism96 <josy6112@gmail.com>
2021-11-18 05:41:46 +0100

    different tap if user login

--- frontend/src/components/Categories.js
+++ frontend/src/components/Categories.js
@@ -1,52 +1,56 @@
-import React, { useState } from "react";
+import React, { Component } from 'react';
 
-const items = [
-  {
-    title: "Excepteur sint occaecat",
-    content:
-      "Morbi tristique senectus et netus et malesuada. Eleifend quam adipiscing vitae proin sagittis nisl rhoncus mattis rhoncus. Morbi tincidunt ornare massa eget egestas purus viverra. Ut tristique et egestas quis ipsum. Nunc faucibus a pellentesque sit amet porttitor eget. Praesent elementum facilisis leo vel fringilla est ullamcorper eget nulla. ",
-  },
-  {
-    title: "Duis aute irure dolor",
-    content:
-      "Augue interdum velit euismod in pellentesque massa placerat. Id aliquet lectus proin nibh nisl condimentum id venenatis. Non enim praesent elementum facilisis leo vel fringilla. Semper eget duis at tellus at urna condimentum mattis pellentesque. ",
-  },
-  {
-    title: "Ut enim ad minim",
-    content:
-      "Lacus luctus accumsan tortor posuere ac ut consequat semper. Suscipit adipiscing bibendum est ultricies integer quis auctor. Nunc sed velit dignissim sodales ut. Vestibulum morbi blandit cursus risus at ultrices mi. Netus et malesuada fames ac turpis egestas integer.",
-  },
-];
+import '../css/Index.css';
+const url = '/api/image';
 
-const Categories = () => {
-  const [activeTab, setActiveTab] = useState(null);
+class Home extends Component {
+  constructor(props) {
+    super(props);
+ 
+    this.state = {
+      hits: [],
+    };
+  }
+ 
+  componentDidMount() {
+    fetch(url)
+      .then(response => response.json())
+      .then(data => this.setState({ hits: data }));
+  }
 
-  const onTitleClick = (index) => {
-    setActiveTab(index);
-  };
-
-  const renderedItems = items.map((item, index) => {
-    //  if current index is same as active tab, add class "active"
-    const activeStatus = index === activeTab ? 'active' : '';
-
+  render() {
+    const { hits } = this.state;
     return (
-      <React.Fragment key={item.title}>
-        <div className={`title ${activeStatus}`} onClick={() => onTitleClick(index)}>
-          <i className="dropdown icon"></i>
-          {item.title}
-        </div>
-        <div className={`content ${activeStatus}`}>
-          <p>{item.content}</p>
-        </div>
-      </React.Fragment>
-    );
-  });
-
-  return (
-    <div className="ui styled accordion">
-      {renderedItems}
-    </div>
-  );
-};
-
-export default Categories;
+          <div class="container">
+            <div class="cards card1">
+              <p>Bugs</p>
+              <div class="bottom-bar"><a>IMAGE LINK</a></div>
+            </div>
+            <div class="cards card2">
+              <p>Richard</p>
+              <div class="bottom-bar"><a>IMAGE LINK</a></div>
+            </div>
+            <div class="cards card3">
+              <p>Gwen</p>
+              <div class="bottom-bar"><a>IMAGE LINK</a></div>
+            </div>
+            <div class="cards card1">
+              <p>Bugs</p>
+              <div class="bottom-bar"><a>IMAGE LINK</a></div>
+            </div>
+            <div class="cards card2">
+              <p>Richard</p>
+              <div class="bottom-bar"><a>IMAGE LINK</a></div>
+            </div>
+            <div class="cards card3">
+              <p>Gwen</p>
+              <div class="bottom-bar"><a>IMAGE LINK</a></div>
+            </div>
+            {hits.map(hit =>
+              <div class="cards"><img src={hit.value}></img></div>
+            )}
+          </div>
+      )
+  }
+}
+export default Home;
