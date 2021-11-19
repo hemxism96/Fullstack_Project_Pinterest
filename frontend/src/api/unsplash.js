1315674899ee95fd74c8aae685eedd0d50ff83c1
@7xNeil7 <neiltheonly@gmail.com>
2021-11-19 18:52:06 +0100

    photo index + new access key api

--- frontend/src/api/unsplash.js
+++ frontend/src/api/unsplash.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
     baseUrl: "https://api.unsplash.com/",
     headers: {
-        Authorization: "Client-ID vVCDfeXbkdH1pnjuuSuhEXXRk1RSXQPxU4iIqVnJb48"
+        Authorization: "Client-ID 7kuPIS3kFrPCU9QxIlXDGpTYqLgAlj4okbMVMTGgx0w"
     }
 })
